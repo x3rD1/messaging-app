31fd0e7b176490316fd97e4b95944753d45a93a6
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-05 16:02:04 -0800

    create utility for signing token with jwt

---
+++ backend/utils/createToken.js
@@ -0,0 +1,12 @@
+require("dotenv/config");
+const jwt = require("jsonwebtoken");
+
+exports.createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "15m" });
+};
+
+exports.createRefreshToken = (payload) => {
+  return jwt.sign({ sub: payload }, process.env.REFRESH_TOKEN, {
+    expiresIn: "7d",
+  });
+};
