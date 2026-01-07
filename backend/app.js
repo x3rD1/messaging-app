37a57012beb987eafda8c728c6f508d5329372b9
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-06 16:41:18 -0800

    Add cors for frontend to be able to talk to backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
