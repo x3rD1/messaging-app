f8e7b7f9ae19a9ae5b3c3a6b794ad24bd5a29d50
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-07 15:31:41 -0800

    Add protected route to /

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<h1>Welcome!</h1>} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <h1>Welcome!</h1>
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
