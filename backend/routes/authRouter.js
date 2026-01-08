9080b33327071347582688e87d18921d5ed6c920
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-07 17:22:54 -0800

    Add signup validator and handleValidation middleware to signup endpoint

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const authRouter = Router();
 const authController = require("../controllers/authController");
+const { handleValidation } = require("../middlewares/handleValidation");
+const { validateSignup } = require("../validators/signup");
 
 authRouter.post("/login", authController.login);
-authRouter.post("/signup", authController.signup);
+authRouter.post(
+  "/signup",
+  validateSignup,
+  handleValidation,
+  authController.signup
+);
 authRouter.post("/logout", authController.logout);
 authRouter.post("/token/refresh", authController.refreshToken);
 
