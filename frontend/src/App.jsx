136ac19ab12bc5e826eb457554b16730fc08c6f9
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-12 17:33:13 -0800

    Update router inside App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import AuthProvider from "./context/AuthProvider";
-import Router from "./router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./router.jsx";
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <AuthProvider>
-      <Router />
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
