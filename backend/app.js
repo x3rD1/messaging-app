a273f3221c72679d9cbb5642cf0e9afdc3052cda
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-12 18:53:14 -0800

    Update cors origin in io

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use("/chat", chatRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://messaging-app-umber.vercel.app",
     credentials: true,
   },
 });
