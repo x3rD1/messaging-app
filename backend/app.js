231847bd2972935cc1feafd23359735530b5aa7c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-12 17:29:26 -0800

    Add Socket.io and new routers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 
 app.use(
@@ -14,10 +17,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const authRouter = require("./routes/authRouter");
+const usersRouter = require("./routes/usersRouter");
+const chatRouter = require("./routes/chatRouter");
 
 app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/chat", chatRouter);
 
-app.listen(3000, (err) => {
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on("leave-room", (roomId) => {
+    socket.leave(roomId);
+  });
+
+  socket.on("message", ({ roomId, msg, userId }) => {
+    socket.to(roomId).emit("message", {
+      msg,
+      from: userId,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, (err) => {
   if (err) throw err;
-  console.log("App is listening in port 3000");
+  console.log("App + Socket.IO is listening in port 3000");
 });
