03a2cac133739fefceac178e23cd2a68682a2f91
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-13 17:28:03 -0800

    Fix issue where tokenVersion is not incrementing on logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -138,10 +138,6 @@ exports.logout = async (req, res) => {
     where: { id: payload.sub },
   });
 
-  console.log("LOGOUT cookies:", req.cookies);
-  console.log("LOGOUT payload:", payload);
-  console.log("LOGOUT user.tokenVersion:", user.tokenVersion);
-
   if (!user || payload.tokenVersion !== user.tokenVersion) {
     res.clearCookie("jid", { path: "/auth" });
     return res.sendStatus(204);
